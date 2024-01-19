230d022b521f8d8c00e000b79de1d957aa2f6fbd
Mats Johansson <matjoh50@gmail.com>
2024-01-19 13:08:53 +0100

    more dynamic style of code

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ import fs from "fs/promises";
 const app = express();
 
 async function renderPage(response, page) {
+
+    const headerbuf = await fs.readFile(`./templates/header.html`);
+    const headerhtml = headerbuf.toString();
+
+    const footerbuf = await fs.readFile(`./templates/footer.html`);
+    const footerhtml = footerbuf.toString();
+
+
   const currentPath = page == "/" ? "index" : page;
   console.log("Current page: " + currentPath);
   const buf = await fs.readFile(`./static/${currentPath}.html`);
   const html = buf.toString();
 
-  response.send(html);
+  response.send(html.replace('%%header%%', headerhtml).replace('%%footer%%', footerhtml));
 }
 
-// app.get('/', async (request, response) => {
-//     const buf = await fs.readFile('./static/index.html');
-//     const html = buf.toString();
-
-//     response.send(html);
-// });
 
 app.get("/", async (request, response) => {
   renderPage(response, "index");
 });
-// app.get("/index", async (request, response) => {
-//   renderPage("index");
-// });
 
 app.get("/filmer", async (request, response) => {
   renderPage(response, "filmer");
@@ -34,14 +33,16 @@ app.get("/aboutus", async (request, response) => {
   renderPage(response, "aboutus");
 });
 
-app.get("/", async (request, response) => {
-  renderPage(response, "newsevent");
+app.get("/newsevents", async (request, response) => {
+  renderPage(response, "newsevents");
 });
 
 app.use("/static", express.static("./static"));
 app.use("/src", express.static("./src"));
 app.use("/assets", express.static("./assets"));
 app.use("/images", express.static("./images"));
+app.use("/content", express.static("./content"));
+app.use("/template", express.static("./template"));
 
 app.listen(3080);
 

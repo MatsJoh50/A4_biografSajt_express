09757c194f319449fa6f334fa9f202d163360cfa
Mats Johansson <matjoh50@gmail.com>
2024-01-25 13:22:57 +0100

    Handlebars rendersd items

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express, { response } from "express";
 import fs from "fs/promises";
 import {engine} from "express-handlebars";
 
+//Port for express to listen to
+const port = 5080;
+
 const app = express();
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({partialsDir: './templates/partials'}));
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 
@@ -20,43 +23,57 @@ const menuItems = [
         lable: 'Nyheter',
         link: '/newsevents',
     },
-    {
-        lable: 'Sponsorer',
-        link: '/ourSponsors',
-    },
 ]
 
 
+
+//Fetch data from the specified file using the fetchData function
+const data = await fetchData("https://plankton-app-xhkom.ondigitalocean.app/api/movies").catch((error) => console.log(error.message));
+
+//function to fetch data from specific file using fetch API
+async function fetchData(file) {
+  //Use the fetch function to make an async http request
+  const response = await fetch(file);
+  
+  //check if response status is ok
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Something went wrong with the request. Error code: ${response.status}`);
+  }
+}
+
+//Giving movies the value of all the movies in the data variable.
+const impData = data.data.map((obj) =>{
+  return{
+    id: obj.id,
+    ...obj.attributes,
+  }
+});
+
+
 async function renderPage(response, page) {
-/*
-  const menuItemsList = menuItems.map((item) => {
-    return `<a href="${item.link}" class="header__link">${item.lable}</a>`;
-  });
-
-  const menuString = menuItemsList.join('\n');
-
-    const headerbuf = await fs.readFile(`./templates/header.html`);
-    const headerhtml = headerbuf.toString().replaceAll('%%menu%%', menuString);
-
-    const footerbuf = await fs.readFile(`./templates/footer.html`);
-    const footerhtml = footerbuf.toString();
-
-
-  const currentPath = (page == '/') ? "index" : page;
-  console.log("Current page: " + currentPath);
-  const buf = await fs.readFile(`./static/${currentPath}.html`);
-  const html = buf.toString();
-
-  response.send(html.replace('%%header%%', headerhtml).replace('%%footer%%', footerhtml));
-  */
- response.render(page, {
-  menu: menuItems.map(item => {
+  response.render(page, {
+    menu: menuItems.map(item => {
     return{
       lable: item.lable,
       link: item.link,
     };
-  })
- });
+  }),
+  api: impData,
+});
+}
+
+async function renderPageId(response, page, id) {
+  response.render(page, {
+    menu: menuItems.map(item => {
+      return{
+        lable: item.lable,
+        link: item.link,
+      };
+    }),
+    movie: impData[impData.findIndex(item => item.id == id)],
+  });
 }
 
 
@@ -68,16 +85,17 @@ app.get("/filmer", async (request, response) => {
   renderPage(response, "filmer");
 });
 
+app.get("/filmer/:id", async (request, response) => {
+  const id = request.params.id;
+  renderPageId(response, "movieinfo", id);
+});
+
 app.get("/aboutus", async (request, response) => {
   renderPage(response, "aboutus");
 });
 
 app.get("/newsevents", async (request, response) => {
   renderPage(response, "newsevents");
-});
-
-app.get("/ourSponsors", async (request, response) => {
-  renderPage(response, "ourSponsors");
 });
 
 
@@ -88,7 +106,6 @@ app.use("/images", express.static("./images"));
 app.use("/content", express.static("./content"));
 app.use("/template", express.static("./template"));
 
-const port = 5080
 app.listen(port);
 
 console.log("=============================")
